0a0c016aa580c49449566163cc276eeb094684b1
Samantha Holmes <samanthavholmes@gmail.com>
2016-08-06 15:53:46 -0400

    add songs to playlist

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,30 +1,47 @@
 class SongsController < ApplicationController
   def create
+    @tracks = params[:tracks].as_json
+    @genre = params[:genre]
+    track_id = @tracks.first["id"]
+    track = RSpotify::Track.find("#{track_id}")
+    @tracks.delete_at(0)
+    user = RSpotify::User.new(session[:user])
+    playlist = user.playlists.find {|playlist| playlist.name == "our-app-#{@genre}"}
+    playlist.add_tracks!([track])
+    @track = @tracks.first
+    render :show
   end
 
   def show
-    session[:tracks] = []
+    @genre = params[:genre]
     spotify_user = RSpotify::User.new(session[:user])
-    existing_playlist = spotify_user.playlists.find {|playlist| playlist.name == "our-app-#{params[:genre]}"}
-    @playlist = spotify_user.create_playlist!("our-app-#{params[:genre]}") unless existing_playlist
-    category = RSpotify::Category.find("#{params[:genre].downcase.tr(" ", "")}")
+    existing_playlist = spotify_user.playlists.find {|playlist| playlist.name == "our-app-#{@genre}"}
+    @playlist = spotify_user.create_playlist!("our-app-#{@genre}") unless existing_playlist
+    category = RSpotify::Category.find("#{@genre.downcase.tr(" ", "")}")
     playlists = category.playlists
-    tracks = []
+    @tracks = []
     i = 0
     playlists.shuffle.each do |playlist|
       playlist.tracks.shuffle.each do |track|
         next unless track.preview_url
         break if i >= 40
-        tracks[i] = {name: track.name, preview: track.preview_url, artist: track.artists.first.name}
+        @tracks[i] = { name: track.name,
+                       preview: track.preview_url,
+                       artist: track.artists.first.name,
+                       id: track.id,
+                       img: track.album.images.first["url"]
+                    }
         i += 1
       end
     end
-    # session[:songs] = RSpotify::Recommendations.generate(seed_genres: ['blues', 'country'])
-    # session[:songs] = RSpotify::Recommendations.generate(seed_genres: ["#{params[:genre]}"])
-    # session[:tracks] = tracks.uniq!
-    @track = tracks.shuffle.first
+    @track = @tracks.shuffle.first
   end
 
   def destroy
+    @tracks = params[:tracks].as_json
+    @genre = params[:genre]
+    @tracks.delete_at(0)
+    @track = @tracks.first
+    render :show
   end
 end

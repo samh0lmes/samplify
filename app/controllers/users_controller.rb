687eff1d26d35f3fcbfc399b271e63cfe7d77817
Samantha Holmes <samanthavholmes@gmail.com>
2016-08-06 00:17:52 -0400

    successfully add playlists directly to spotify account and receive song recommendations based on genre

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
   def spotify
     spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
+    # playlist = spotify_user.create_playlist!('this-created-with-oauth')
+    # tracks = RSpotify::Track.search('Video Games')
+    # playlist.add_tracks!(tracks)
+    recommendations = RSpotify::Recommendations.generate(seed_genres: ['blues', 'country'])
     binding.pry
-    render :show, locals: {user: spotify_user}
+    render :show, locals: {user: spotify_user, playlists: spotify_user.playlists, recommendations: recommendations}
   end
 end

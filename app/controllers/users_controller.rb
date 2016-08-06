568924d56913063905d7edee2a2aac29c68ed7bc
Samantha Holmes <samanthavholmes@gmail.com>
2016-08-06 14:43:57 -0400

    Get tracks from playlists, add songs controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     # playlist = spotify_user.create_playlist!('this-created-with-oauth')
     # tracks = RSpotify::Track.search('Video Games')
     # playlist.add_tracks!(tracks)
-    recommendations = RSpotify::Recommendations.generate(seed_genres: ['blues', 'country'])
+    redirect_to '/genres'
   end
 
   def destroy

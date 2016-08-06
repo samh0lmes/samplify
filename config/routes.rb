2ea159b753b0ddcc44ac7f5a63e049f43c2c024e
Samantha Holmes <samanthavholmes@gmail.com>
2016-08-06 12:51:52 -0400

    Successfully access genre name and images

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get '/auth/spotify/callback', to: 'users#spotify'
   delete '/user', to: 'users#destroy'
+  resources :genres, only: [:index]
 end

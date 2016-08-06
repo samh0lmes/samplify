7ec60f6e88caee31e1f4fb41db79f3f87ab689d4
Samantha Holmes <samanthavholmes@gmail.com>
2016-08-06 12:25:36 -0400

    Add logout button to layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,16 @@
 class UsersController < ApplicationController
   def spotify
     spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
+    session[:user] = spotify_user
     # playlist = spotify_user.create_playlist!('this-created-with-oauth')
     # tracks = RSpotify::Track.search('Video Games')
     # playlist.add_tracks!(tracks)
     recommendations = RSpotify::Recommendations.generate(seed_genres: ['blues', 'country'])
-    binding.pry
     render :show, locals: {user: spotify_user, playlists: spotify_user.playlists, recommendations: recommendations}
+  end
+
+  def destroy
+    session.clear
+    redirect_to :root
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   get '/auth/spotify/callback', to: 'users#spotify'
+  delete '/user', to: 'users#destroy'
 end

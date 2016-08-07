e963d86ed05083e48b4d91e7775cec5d9d523cc5
Shawn O'Connor <bartandfosto@gmail.com>
2016-08-06 20:01:26 -0400

    add loading screen

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,9 @@
 //= require materialize/extras/nouislider
 //= require jquery_ujs
 //= require_tree .
+
+$(document).ready(function() {
+	$(".card-image").click(function() {
+		$(".overlay, .overlay-message").show();
+	})
+});
